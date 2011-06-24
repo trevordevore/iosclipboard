ee1d97b1cf108fd19084e8b0821007536f5fd7ef
Trevor DeVore <trevor.devore@bluemangolearning.com>
2011-06-24 10:35:59 -0400

    Added copyTextToClipboard

--- iosclipboard/iosclipboard.mm
+++ iosclipboard/iosclipboard.mm
@@ -3,5 +3,19 @@
 //  iosclipboard
 //
 //  Created by Trevor K DeVore on 6/24/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Blue Mango Learning Systems. All rights reserved.
 //
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIPasteboard.h>
+//#include <LiveCode.h> ## this doesn't use any LiveCode APIs
+
+////////////////////////////////////////////////////////////////////////////////
+
+// NSData is a UTF8 string.
+void copyTextToClipboard(NSData* textToCopy)
+{
+    //NSLog(@"copyTextToClipboard called");
+    UIPasteboard *gpBoard = [UIPasteboard generalPasteboard];   
+    [gpBoard setData:textToCopy forPasteboardType:@"public.utf8-plain-text"];
+}
